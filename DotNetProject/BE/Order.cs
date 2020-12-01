80b806eba637665e99be4dfe52d069649f49cb50
shacharMarkovich <234shachar@gmail.com>
2020-12-01 20:36:01 +0200

    Add Class Libraries

---
+++ DotNetProject/BE/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BE
+{
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public DateTime _orderDate { get; set; }
+        public Product[] _productsList { get; set; }
+
+        public int GetOrderPrice()
+        {
+            return -1;
+        }
+
+    }
+}
---
+++ DotNetProject/BL/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BL
+{
+    public class Class1
+    {
+
+    }
+}
