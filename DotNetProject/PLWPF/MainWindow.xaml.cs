63a64e89d76a89a146b8c994560913f7ae4219ce
Naor <mamannaor@gmail.com>
2021-01-13 23:16:57 +0200

    PL init

--- DotNetProject/PLWPF/MainWindow.xaml.cs
+++ DotNetProject/PLWPF/MainWindow.xaml.cs
@@ -25,4 +25,35 @@ namespace PLWPF
             InitializeComponent();
         }
     }
+    public class ControlPagesData : List<ControlInfoDataItem>
+    {
+        public ControlPagesData()
+        {
+          //  AddPage(typeof(ControlPalettePage), "Control Palette");
+
+        }
+
+        private void AddPage(Type pageType, string displayName = null)
+        {
+            Add(new ControlInfoDataItem(pageType, displayName));
+        }
+    }
+
+    public class ControlInfoDataItem
+    {
+        public ControlInfoDataItem(Type pageType, string title = null)
+        {
+            PageType = pageType;
+            Title = title ?? pageType.Name.Replace("Page", null);
+        }
+
+        public string Title { get; }
+
+        public Type PageType { get; }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
 }
