3ec2ce812d67b704ce6aff659a5d64845d9f74f7
ShacharMarkovich <ShacharMarkovich@users.noreply.github.com>
2021-02-22 14:31:33 +0200

    Add BoolToFontWeightConverter

---
+++ DotNetProject/PLApp/Converters/BoolToFontWeightConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PLApp.Converters
+{
+    public class BoolToFontWeightConverter : DependencyObject, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => ((bool)value) ? FontWeights.Bold : FontWeights.Normal;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Binding.DoNothing;
+    }
+}
