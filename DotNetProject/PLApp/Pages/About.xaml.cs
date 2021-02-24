f811478b54ccf7a85cd4774e34c2cbfbe80c0f0e
ShacharMarkovich <ShacharMarkovich@users.noreply.github.com>
2021-02-24 10:31:54 +0200

    Update Buttons in about page

--- DotNetProject/PLApp/Pages/About.xaml.cs
+++ DotNetProject/PLApp/Pages/About.xaml.cs
@@ -21,26 +21,15 @@ namespace PLApp.Pages
     /// </summary>
     public partial class About : UserControl
     {
-        public String Title { get; set; }
         public About()
         {
-            Title = "About";
             InitializeComponent();
         }
 
-        private void GoogleDrive_Click(object sender, RoutedEventArgs e)
-        {
-            Process.Start("https://drive.google.com/drive/u/2/folders/1dffosvvP2Vk5JD3TvOfzXBt0J63SO9Il");
-        }
+        private void GoogleDrive_Click(object sender, RoutedEventArgs e) => Process.Start("https://drive.google.com/drive/u/2/folders/1dffosvvP2Vk5JD3TvOfzXBt0J63SO9Il");
 
-        private void Github_Click(object sender, RoutedEventArgs e)
-        {
-            Process.Start("https://github.com/ShacharMarkovich/SAMS");
-        }
+        private void Github_Click(object sender, RoutedEventArgs e) => Process.Start("https://github.com/ShacharMarkovich/SAMS");
 
-        private void Paypal_Click(object sender, RoutedEventArgs e)
-        {
-            Process.Start("https://paypal.me/LevSAMS?locale.x=he_IL");
-        }
+        private void Paypal_Click(object sender, RoutedEventArgs e) => Process.Start("https://paypal.me/LevSAMS?locale.x=he_IL");
     }
 }
